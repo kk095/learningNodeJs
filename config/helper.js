c600d1d8da74f4d44ea624129ba10fe4886441f7
kk <kk41110002@gmail.com>
2022-03-15 13:39:57 +0530

    d

--- config/helper.js
+++ config/helper.js
@@ -7,7 +7,6 @@ module.exports = (app)=>{
         if(env.name=="development"){
             return filepath;
         }
-    console.log('/'+ JSON.parse(fs.readFileSync(path.join(__dirname,"../public/assets/rev-manifest.json")))[filepath])
         return '/'+ JSON.parse(fs.readFileSync(path.join(__dirname,"../public/assets/rev-manifest.json")))[filepath];
         
     }

8fd9d81208afb669ff3a19f975ffe2e66101570f
kk <kk41110002@gmail.com>
2022-03-15 11:15:14 +0530

    helper file remove console

--- config/helper.js
+++ config/helper.js
@@ -8,7 +8,7 @@ module.exports = (app)=>{
             return filepath;
         }
 
-    console.log('/'+ JSON.parse(fs.readFileSync(path.join(__dirname,"../public/assets/rev-manifest.json")))[filepath])
+    // console.log('/'+ JSON.parse(fs.readFileSync(path.join(__dirname,"../public/assets/rev-manifest.json")))[filepath])
         return '/'+ JSON.parse(fs.readFileSync(path.join(__dirname,"../public/assets/rev-manifest.json")))[filepath];
         
     }
